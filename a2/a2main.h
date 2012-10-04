7d371f6f6a3e788b8419a6cc4555de2b1aa43053
DHMO <dhmo@dhmo.com>
2012-10-04 09:36:52 +0200

    some more stuff

---
+++ a2/a2main.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   a2main.h
+ * Author: fox
+ *
+ * Created on 04 October 2012, 09:29
+ */
+
+
+struct Vector 
+{
+    float x,y,z;    
+};
+
+Vector* VectorAdd(Vector* v1, Vector* v2);
