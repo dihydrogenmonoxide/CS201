9bb707a3ee2e6036af2c19e59c7784cfaef8a35a
DHMO <dhmo@dhmo.com>
2012-10-04 09:27:06 +0200

    Initialized

---
+++ a2/a2main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   a2main.c
+ * Author: fox
+ *
+ * Created on 04 October 2012, 09:26
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ a2/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: fox
+ *
+ * Created on 04 October 2012, 09:24
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
