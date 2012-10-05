7b31021fb5907656e3287ff84382aefa10adcc41
DHMO <dhmo@dhmo.com>
2012-10-05 11:41:11 +0200

    Added A1

---
+++ ex2/A1/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: fox
+ *
+ * Created on 05 October 2012, 11:37
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
